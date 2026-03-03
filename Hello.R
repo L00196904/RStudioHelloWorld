dafd0c140225286ae521d4c7cb10dcf9fc3029a5
Sakun Mahasuriyage - STUDENT <l00196904@atu.ie>
2026-03-03 15:04:00 +0000

    data types

--- Hello.R
+++ Hello.R
@@ -1,6 +1,54 @@
-a <- 10
-b <- 15
+#Print
+print('Hello World')
 
+#Assign variables
+a <- 2
+b <- 4
+
+#print variables without print statement
 a 
 b
+
+#Mathematical operations
+a+b
+a-b
+a/b
+a*b
+a^b
+
+#Assign multiple variables in single line
+x <- y <- 10
+x
+y
+
+#Compute variables in single line
+(z <- x + y)
+
+#Remove unused variables
+rm(a)
+
+#Find the data type
+(b <- 'Data Science')
+class(b)
+
+class(x)
+
+(c <- TRUE)
+class(c)
+
+
+num_1 <- 10
+num_2 <- 10.5
+
+class(num_1)
+class(num_2)
+
+# checking values are numeric in batch
+is.numeric(num_1)
+is.numeric(num_2)
+
+
+
+
+
 
